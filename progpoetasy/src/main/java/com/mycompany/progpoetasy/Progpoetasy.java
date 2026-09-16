d0b0ae2642bd584bd9ee4bb5cc87f7e26bacdfc3
Student <Student@LabVM2049939.0eoi3jg3lycebpweedsz2vtxnb.jnbx.internal.cloudapp.net>
2026-09-16 14:08:09 +0200

    Changing while true

---
+++ progpoetasy/src/main/java/com/mycompany/progpoetasy/Progpoetasy.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.progpoetasy;
+
+/**
+ *
+ * @author Student
+ */
+public class Progpoetasy {
+
+    public static void main(String[] args) {
+      
+    }
+}
