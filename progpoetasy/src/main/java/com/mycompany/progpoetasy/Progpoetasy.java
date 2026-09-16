d919e1d0497cf73edc8bf44484b0add80830a439
Student <Student@LabVM2049939.0eoi3jg3lycebpweedsz2vtxnb.jnbx.internal.cloudapp.net>
2026-09-16 14:13:51 +0200

    Changing while true

--- progpoetasy/src/main/java/com/mycompany/progpoetasy/Progpoetasy.java
+++ progpoetasy/src/main/java/com/mycompany/progpoetasy/Progpoetasy.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.progpoetasy;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Student
@@ -12,5 +14,54 @@ public class Progpoetasy {
 
     public static void main(String[] args) {
       
+    //Declarations of variables
+    String name;
+    String lastName;
+    
+    //Use a Scanner to acceptr user registration input
+    Scanner myRegister = new Scanner(System.in);
+    System.out.println("Enter your first name: ");
+    name = myRegister.nextLine().trim();
+    System.out.println("Enter your last name: ");
+    lastName = myRegister.nextLine().trim();
+    
+    //Registering the a correctly formatted usename
+    for (int i=0; i != -1; i++){
+        Boolean regUsername = checkUserName();
+        if (regUsername == true){
+            System.out.println("Username successsfully captured ");
+            break;
+        }
+        else{
+            System.out.println("Username not correctly formatted: ");
+            System.out.print("Ensure that username ");
+            System.out.print("contains an underscore, ");
+            System.out.print("and is no more than 5 characters in length.");
+        }
+    }
+    
+    }
+    //Check if username is correctly formatted
+    public static Boolean checkUserName(){
+        //Declaration of variables
+        Boolean results;
+        results = true;
+        String username;
+        int i;
+        
+        Scanner myRegister = new Scanner(System.in);
+        System.out.print("Enter a username: ");
+        username = myRegister.nextLine().trim();
+        
+        // Checking if username meets the criteria
+        if (username.length() >= 6){
+            return false;
+        }
+        if (username.contains("_")){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
